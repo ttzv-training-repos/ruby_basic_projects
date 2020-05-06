5fddbdacc8bc5661b5fc53a9f1aab2e3083681c8
ttzv <teyed1fy@gmail.com>
2020-05-06 10:03:05 +0200

    finish stockpicker

--- stockpicker.rb
+++ stockpicker.rb
@@ -1,33 +1,51 @@
 def stock_picker(prices)
-  best_pair(prices)
+  most_gain = uniq_pairs_2(prices).reduce do |best, price| 
+    if pair_diff(best) < pair_diff(price)
+      best = price
+    end
+    best
+  end
+  return [most_gain[:buy_index], most_gain[:sell_index]]
 end
 
-def uniq_pairs(arr)
-  pairs = Hash.new
-  arr.each_with_index do |first_el, f_index|
-    arr.each_with_index do |sec_el, s_index|
-      if first_el != sec_el && s_index > f_index
-        pairs[[f_index, s_index]]=[first_el, sec_el]
+def pair_diff(pair)
+ return pair[:sell_price] - pair[:buy_price]
+end
+
+def uniq_pairs_2(array)
+  excluded_mins = []
+  pairs = Array.new
+  loop do
+    cmin = filtered_min(array, excluded_mins)
+    break if cmin[:el] == array.max
+    array.each_with_index do |el, i|
+      if i > cmin[:index] && el > cmin[:el]
+        pairs.push({
+          buy_index: cmin[:index],
+          sell_index: i,
+          buy_price: cmin[:el],
+          sell_price: el
+        })
       end
     end
+    excluded_mins.push(cmin[:el])
   end
   pairs
 end
 
-def best_pair(pairs) 
-  uniq_pairs(pairs).reduce do |acc, k| 
-    p k
+def filtered_min(array, filter_arr)
+  min = array.max
+  i = 0
+  array.each_with_index do |el,  index|
+    if el < min && !filter_arr.include?(el)
+      min = el
+      i = index
+    end
   end
+  return {index: i, el: min} 
 end
 
-def pair_diff(pair)
-  if pair.length == 2
-    return pair.first - pair.last
-  end
-  return 0
-end
-
-#stock_picker([17,3,6,9,15,8,6,1,10])
-
-best_pair([17,3,6,9,15,8,6,1,10])
-
+stocks = [17,3,6,9,15,8,6,1,10]
+stocks_1 = [44, 43, 66, 40, 66, 40, 90, 41]
+puts stock_picker(stocks)
+puts stock_picker(stocks_1)
